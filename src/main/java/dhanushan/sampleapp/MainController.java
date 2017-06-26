c2230c83b1492dd7fb9e3036a27e772381918493
Dhanushan <dhanushan@duosoftware.com>
2017-06-26 23:42:21 +0530

    init

---
+++ src/main/java/dhanushan/sampleapp/MainController.java
@@ -0,0 +1,28 @@
+package dhanushan.sampleapp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+/**
+ * Created by Malinda_07654 on 2/9/2016.
+ */
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> inbound(HttpServletRequest request) {
+
+
+        return new ResponseEntity<String>("Done", HttpStatus.OK);
+
+    }
+
+
+
+}
